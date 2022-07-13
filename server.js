3b2868f4c1c82eb8dcda7eaae3365d369628fd4c
David Souther <davidsouther+github@gmail.com>
2022-07-12 19:12:44 -0700

    Added notes on handlers nad middleware

--- server.js
+++ server.js
@@ -3,10 +3,12 @@
 const express = require('express');
 
 const hello = (req, res) => {
+  // console.log(Date.now(), req.url);
   res.status(200).send('Hello, World');
 };
 
 const data = (req, res) => {
+  // console.log(Date.now(), req.url);
   res.status(200).send({
     name: 'David',
     role: 'Instructor',
@@ -15,8 +17,24 @@ const data = (req, res) => {
 
 const app = express();
 
-app.get('/', hello);
+const logger = (req, res, next) => {
+  console.log(Date.now(), req.url);
+  next();
+};
 
+// app.use(something - a middleware!);
+
+// what is a handler?
+
+function handler(req, res, next) {
+  // request: query, endpoints, URL - things the browser sends to the server
+  // response: status, body - what you give back to the server
+  // next: a function passed to the middleware, to pass control to the "next" handler
+}
+
+app.use(logger);
+
+app.get('/', hello);
 app.get('/data', data);
 
 // ---
