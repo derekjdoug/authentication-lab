f25a8e4e613259d4eef76ad4401ba19377606f1c
David Souther <davidsouther+github@gmail.com>
2022-07-12 17:31:07 -0700

    Bumped prettierconfig

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -8,34 +8,34 @@
 //   it is a single test case
 // test setup
 
-const supertest = require("supertest");
-const server = require("../server.js");
+const supertest = require('supertest');
+const server = require('../server.js');
 
 const request = supertest(server.app);
 
-describe("Node Server", () => {
-    it("says hello world", async () => {
-        // set up the test so it can do a thing
-        // Prepare the server
-        // (see above)
+describe('Node Server', () => {
+  it('says hello world', async () => {
+    // set up the test so it can do a thing
+    // Prepare the server
+    // (see above)
 
-        // perform an action, that does the thing
-        // request the / route
-        const response = await request.get("/"); // The response is a promise
+    // perform an action, that does the thing
+    // request the / route
+    const response = await request.get('/'); // The response is a promise
 
-        // assert or expect the result of the action
-        // expect the / route to respond with hello
-        expect(response.status).toBe(200);
-        expect(response.text).toBe("Hello, World");
+    // assert or expect the result of the action
+    // expect the / route to respond with hello
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello, World');
+  });
+
+  it('returns some data', async () => {
+    const response = await request.get('/data');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      name: 'David',
+      role: 'Instructor',
     });
-
-    it("returns some data", async () => {
-        const response = await request.get("/data");
-
-        expect(response.status).toBe(200);
-        expect(response.body).toEqual({
-            name: "David",
-            role: "Instructor",
-        });
-    });
+  });
 });
--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 
 const hello = (req, res) => {
-    res.status(200).send("Hello, World");
+  res.status(200).send('Hello, World');
 };
 
 const data = (req, res) => {
-    res.status(200).send({
-        name: "David",
-        role: "Instructor",
-    });
+  res.status(200).send({
+    name: 'David',
+    role: 'Instructor',
+  });
 };
 
 const app = express();
 
-app.get("/", hello);
+app.get('/', hello);
 
-app.get("/data", data);
+app.get('/data', data);
 
 // ---
 
 function start(port) {
-    app.listen(port, () => console.log(`Server listening on port ${port}`));
+  app.listen(port, () => console.log(`Server listening on port ${port}`));
 }
 
 module.exports = {
-    app,
-    start,
+  app,
+  start,
 };
